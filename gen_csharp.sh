944436b43c40dec7c872e4b71a3b3dfa6548be4e
ambientlight <primary.taras.vozniuk@gmail.com>
2021-06-30 18:53:03 +0800

    injected additional shared source

--- gen_csharp.sh
+++ gen_csharp.sh
@@ -23,7 +23,7 @@ find specification/maps/data-plane/*/* -name "readme.md" -print | while read REA
   README_FPATH=$(dirname $README_PATH)
   SRCPATH=$(cat $README_FPATH/readme.csharp.md | grep '^\s*output-folder:' | sed "s/output-folder://" | xargs dirname)
   SERVICE_NAME=$(dirname $SRCPATH | xargs basename)
-  TRACK2_COMMAND='npx autorest@3.2.1 --skip-csproj --skip-upgrade-check --version=3.4.5 '"$README_PATH"' --use='"$(realpath ~)"'/.nuget/packages/microsoft.azure.autorest.csharp/3.0.0-beta.20210615.2/buildMultiTargeting/../tools/netcoreapp3.1/any/ --output-folder='"$(realpath ../azure-sdk-for-net)"'/sdk/maps/'"$SERVICE_NAME"'/src/Generated --namespace='"$SERVICE_NAME"' --clear-output-folder=true --shared-source-folders="'"$(realpath ../azure-sdk-for-net/)"'/eng//../sdk/core/Azure.Core/src/Shared/;'"$(realpath ~)"'/.nuget/packages/microsoft.azure.autorest.csharp/3.0.0-beta.20210615.2/buildMultiTargeting/../content/Generator.Shared/"'
+  TRACK2_COMMAND='npx autorest@3.2.1 --skip-csproj --skip-upgrade-check --version=3.4.5 '"$README_PATH"' --use='"$(realpath ~)"'/.nuget/packages/microsoft.azure.autorest.csharp/3.0.0-beta.20210615.2/buildMultiTargeting/../tools/netcoreapp3.1/any/ --output-folder='"$(realpath ../azure-sdk-for-net)"'/sdk/maps/'"$SERVICE_NAME"'/src/Generated --namespace='"$SERVICE_NAME"' --clear-output-folder=true --shared-source-folders="'"$(realpath ../azure-sdk-for-net/)"'/eng//../sdk/core/Azure.Core/src/Shared/;'"$(realpath ~)"'/.nuget/packages/microsoft.azure.autorest.csharp/3.0.0-beta.20210615.2/buildMultiTargeting/../content/Generator.Shared/" --public-clients'
   
   echo $TRACK2_COMMAND
   eval $TRACK2_COMMAND
@@ -31,11 +31,18 @@ find specification/maps/data-plane/*/* -name "readme.md" -print | while read REA
   cp ../azure-sdk-for-net/sdk/template/Azure.Template/src/Azure.Template.csproj ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
   cp ../azure-sdk-for-net/sdk/template/Azure.Template/Directory.Build.props ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/
 
+  # update meta
   sed -i "s/<Version>.*<\/Version>/<Version>$VERSION<\/Version>/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
   sed -i "s/<AssemblyTitle>.*<\/AssemblyTitle>/<AssemblyTitle>Azure Maps $SERVICE_NAME<\/AssemblyTitle>/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
   sed -i "s/<PackageTags>.*<\/PackageTags>/<PackageTags>Azure;Azure Maps;Maps $SERVICE_NAME<\/PackageTags>/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
   sed -i "s/<Description>.*<\/Description>/<Description>Azure Maps $SERVICE_NAME<\/Description>/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
   sed -i "s/<\/PropertyGroup>/<NoWarn>\$(NoWarn);AZC0001;AZC0012<\/NoWarn><\/PropertyGroup>/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
+  # inject additional shared source
+  sed -z "s/<ItemGroup>\n\s*<Compile/<ItemGroup>\n    <Compile Include=\"\$(AzureCoreSharedSources)Argument.cs\" Link=\"Shared\%(RecursiveDir)\%(Filename)%(Extension)\" \/>\n    <Compile Include=\"\$(AzureCoreSharedSources)AzureKeyCredentialPolicy.cs\" Link=\"Shared\%(RecursiveDir)\%(Filename)%(Extension)\" \/>\n    <Compile/" ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj > ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.tmp.csproj
+  mv ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.tmp.csproj ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/$SERVICE_NAME.csproj
+
+  # <Compile Include="$(AzureCoreSharedSources)Argument.cs" Link="Shared\%(RecursiveDir)\%(Filename)%(Extension)" />
+  # <Compile Include="$(AzureCoreSharedSources)AzureKeyCredentialPolicy.cs" Link="Shared\%(RecursiveDir)\%(Filename)%(Extension)" />
 
   # the build somehow add condiitional dependencies if autorest.md is present, otherwise I cannot explain build failing without empty autorest.md...
   touch ../azure-sdk-for-net/sdk/maps/$SERVICE_NAME/src/autorest.md
